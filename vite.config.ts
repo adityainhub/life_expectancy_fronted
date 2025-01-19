f412412b3ca8a71e98d6e97c8957850be741839f
adityainhub <k.aditya9599@gmail.com>
2025-01-19 15:50:53 +0530

    corrected backened url path

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://web-production-69fc2.up.railway.app/',
+        target: 'https://web-production-69fc2.up.railway.app',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
